84430fcc1adfe3a2f2aa8942635f44745cec3593
Amin Mohamed Ajani <me@allrightamin.xyz>
2016-03-17 18:22:06 +0530

    set up scaffold

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+app.set('views', ['./public','./public/partials/']);
+app.set('view engine', 'ejs');
+
+
 app.get('/', function(req, res){
-    res.send('Hola!');
+    res.render('index');
+});
+
+app.get('/p', function(req,res){
+    res.render('partials1');
 });
 
 app.listen(port, function(){
