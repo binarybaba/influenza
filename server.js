0477fa07cba1917a4023f0be17ebd739f5214eae
Amin Mohamed Ajani <me@allrightamin.xyz>
2016-03-18 00:16:00 +0530

    remove routes. keeping it simple for now

--- server.js
+++ server.js
@@ -3,17 +3,18 @@ var app = express();
 var port = process.env.PORT || 3000;
 
 app.use(express.static('public'));
-app.set('views', ['./public','./public/partials/']);
-app.set('view engine', 'ejs');
+/*app.set('views', ['./public','./public/partials/']);*/ 
+/*Commenting ^this out because we dont need public partials to be rendered server side and it's causing a lot of shit with angular + ui.router*/
 
+app.set('views', './views');
+/*Setting the views here. So we've just moved ./partials/index.ejs to its own views folder at ./views/ */
+app.set('view engine', 'ejs');
 
 app.get('/', function(req, res){
     res.render('index');
 });
 
-app.get('/p', function(req,res){
-    res.render('partials1');
-});
+
 
 app.listen(port, function(){
     console.log("Listening on port "+port);
