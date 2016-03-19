689e02e93119c0eb20e33b25dc47ebdc2b80648d
Amin Mohamed Ajani <me@allrightamin.xyz>
2016-03-19 14:53:07 +0530

    add index routes

--- controllers/routes/index.js
+++ controllers/routes/index.js
@@ -1,29 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res){
-    res.render('index',{
-        site:'http://allrightamin.xyz',
+router.get('/', function(req,res){
+    res.render('index', {
+         site:'http://allrightamin.xyz',
         me:'Amin Mohamed Ajani'
     });
 });
 
 module.exports = router;
-
-/*var express = require('express');
-var app = express();
-
-exports.partials = function(req, res){
-    var filename = req.params.filename;
-    if(filename){
-        res.render('partials/'+filename);
-    }
-    else{
-        return;
-    }
-        
-};
-
-exports.index = function(req, res){
-    res.render('index');
-};*/
