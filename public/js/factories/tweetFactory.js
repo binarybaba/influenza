8591a60b7e64697fade12baa52fc362b625e4830
Amin Mohamed Ajani <me@allrightamin.xyz>
2016-03-27 17:17:31 +0530

    remove dummy data and add getInfluencer service and push influencerList serviec

--- public/js/factories/tweetFactory.js
+++ public/js/factories/tweetFactory.js
@@ -1,22 +1,12 @@
 angular.module('influenza')
     .factory('tweetFactory', function () {
         var influencerList = [];
-        var idList = [];
-        var Influencer = {
-            id: '',
-            name: '',
-            handle: '',
-            count: 0,
-            tweetText: '',
-        };
-
         var service = {};
         service.getInfluencers = function () {
-
-            //need id and handle in an object so that we can access it via a new controller of a new view
-            /* return track.influencers;*/
-            return Influencer; //dummy
-
+            return influencerList;
+        };
+        service.pushInfluencerList = function (list) {
+            influencerList = list;
         }
         return service;
 
